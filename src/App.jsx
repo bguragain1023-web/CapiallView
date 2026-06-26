2be9d13a4ef7a63b2d02d524ae62128cbd2edd54
Brazesh Guragain <b.guragain1023@gmail.com>
2026-06-26 21:19:24 +1000

    react-router-dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import { ToastContainer, toast } from "react-toastify";
+import { Routes, Route } from "react-router";
+import Login from "./pages/Login";
+
 function App() {
-  toast.success("congratulation");
   return (
     <>
-      Hello world
-      <Button variant="primary">Primary</Button>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
       <ToastContainer />
     </>
   );
